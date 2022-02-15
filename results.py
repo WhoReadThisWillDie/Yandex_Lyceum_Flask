37ab2bcedf3159cb90562bfde49c726bdf958f41
WhoReadThisWillDie <msadkoff@mail.ru>
2022-02-15 19:38:44 +0300

    results

--- results.py
+++ results.py
@@ -69,5 +69,31 @@ def promotion_image():
                     </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                            <title>Результаты</title>
+                        </head>
+                        <body>
+                            <h1>Результаты отбора на участие в миссии</h1>
+                            <h2>Претендент: {nickname}</h2>
+                            <div class='alert alert-primary' role='alert'>
+                                Поздравляем! Вы прошли {level} этап отбора!
+                            </div>
+                            <div class='alert alert-success' role='alert'>
+                                Ваш рейтинг составляет {rating}!
+                            </div>
+                            <div class='alert alert-warning' role='alert'>
+                                Желаем удачи!
+                            </div>
+                        </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
