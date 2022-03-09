505fb04f7c343892b447642f6507b416cc7a1128
WhoReadThisWillDie <msadkoff@mail.ru>
2022-03-09 22:17:43 +0300

    list_prof

--- first_template.py
+++ first_template.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
     return render_template('base.html', title=title)
+
+
+@app.route('/list_prof/<param>')
+def list_prof(param):
+    list_ = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+             'климталог', 'специалист по радиационной защите', 'астрогеолог', 'гляцеолог', 'инженер жизнеобеспечения',
+             'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('list_prof.html', list=list_, param=param)
 
 
 if __name__ == '__main__':
