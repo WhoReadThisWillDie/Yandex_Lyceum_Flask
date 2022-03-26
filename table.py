1c7de837254fac3bae4ec51f892d0b82505fc972
WhoReadThisWillDie <msadkoff@mail.ru>
2022-03-27 00:34:15 +0300

    training

--- table.py
+++ table.py
@@ -13,6 +13,11 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 @app.route('/list_prof/<param>')
 def list_prof(param):
     list_ = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
